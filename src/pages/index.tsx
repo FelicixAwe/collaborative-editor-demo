79f11e0d11057e5925edc1346ecbe44eb5bb224a
FelicixAwe <yiquxiao@gmail.com>
2024-03-12 00:17:30 -0700

    Take care of delete/add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -58,20 +58,25 @@ export default function Home() {
     const newText = e.target.value;
     const diffs = findDiff(content, newText);
     console.log("About to send an update");
+    console.log("diffs: ", diffs);
 
     if (ydocRef.current && diffs.length > 0) {
       const ytext = ydocRef.current.getText("sharedText");
       ydocRef.current.transact(() => {
-        diffs.forEach((diff) => {
-          if (diff.type === "add" && typeof diff.value === "string") {
-            ytext.insert(diff.position, diff.value);
-          } else if (
-            diff.type === "delete" &&
-            typeof diff.length === "number"
-          ) {
-            ytext.delete(diff.position, diff.length);
-          }
-        });
+        ytext.insert(
+          ytext.length - 1,
+          e.target.value[e.target.value.length - 1],
+        );
+        // diffs.forEach((diff) => {
+        //   if (diff.type === "add" && typeof diff.value === "string") {
+        //     ytext.insert(diff.position, diff.value);
+        //   } else if (
+        //     diff.type === "delete" &&
+        //     typeof diff.length === "number"
+        //   ) {
+        //     ytext.delete(diff.position, diff.length);
+        //   }
+        // });
       });
       const update = Y.encodeStateAsUpdate(ydocRef.current);
       if (socketRef.current) {
