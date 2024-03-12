80aed21f81b9b7011d304cbcf655e54ab7602d00
FelicixAwe <yiquxiao@gmail.com>
2024-03-11 23:10:30 -0700

    Enable Azure socket

--- functions/server.js
+++ functions/server.js
@@ -1,49 +1,38 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8080; // Use a different port from your Next.js application
-const socketIO = require("socket.io");
-const http = require("http");
+const port = process.env.PORT || 8080;
+const { WebPubSubServiceClient } = require("@azure/web-pubsub");
 const { nanoid } = require("nanoid");
 const cors = require("cors");
 
-const server = http.createServer(app);
-const io = socketIO(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
+const connectionString =
+  "Endpoint=https://voop-socket-test.webpubsub.azure.com;AccessKey=XBj9LaNaLsfLywuaKFpKeL8OMbrUsx+zBGAw17DBNAw=;Version=1.0;";
+const hubName = "TestHub";
+const pubsub = new WebPubSubServiceClient(connectionString, hubName);
 
 app.use(express.json());
 app.use(cors());
-// app.get("/negotiate", (req, res) => {
-//   const userID = nanoid(4);
-//   io.on("connection", (socket) => {
-//     console.log(`User ${userID} sent connection request.`);
-//     socket.on("disconnect", () => {
-//       console.log("user disconnected");
-//     });
-//   });
-//   res.send("Hello, this is the backend server!");
-// });
-//
-// app.put("/send-message", (req, res) => {});
 
-io.on("connection", (socket) => {
+app.get("/negotiate", async (req, res) => {
   console.log(`A new user connected`);
-  socket.on("update", (update) => {
-    // Broadcast the update to all other connected clients
-    console.log("Received an update: ", update);
-    socket.broadcast.emit("update", update);
+  const userID = nanoid(4);
+  const token = await pubsub.getClientAccessToken({
+    userId: userID,
+    roles: ["webpubsub.sendToAll"],
   });
-  socket.on("disconnect", () => {
-    console.log("A user disconnected");
-  });
+  // console.log("Returning url:", token.url);
+  return res.status(200).json({ url: token.url, userId: userID });
 });
 
-// app.listen(port, () => {
-//   console.log(`Server is running on http://localhost:${port}`);
-// });
-server.listen(port, () => {
+app.put("/send-message", async (req, res) => {
+  // console.log("Received from user: ", req.body);
+  console.log("Received from user");
+  pubsub.sendToAll(req.body, { contentType: "application/json" });
+  // pubsub.sendToAll("Hi there");
+  // pubsub.sendToAll("Hi there", { contentType: "text/plain" });
+  res.json({ message: "Received." });
+});
+
+app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
